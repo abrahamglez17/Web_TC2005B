aa44d1550d0c3ffcea8a49f5dd41d410e9ba30af
José Abraham González Navarro <65511679+abrahamglez17@users.noreply.github.com>
2022-02-23 09:24:47 -0600

    carpeta para actividad 2

---
+++ actividad2/script.js
@@ -0,0 +1,32 @@
+// aqui adentro va a hacer lógica de javascript
+// ahora los comentarios son con dos 
+
+// deckarar variables
+x = 5;
+x= "this is possible somehow"
+console.log("hola mundo menos ")
+function ejemplo(){
+
+    console.log("HOLA MUNDO")
+   //alert("hola mundo alerta")
+}
+
+function boton2(){
+    alert("BOTON 2")
+}
+
+function boton3(){
+    alert("BOTON 3")
+
+    // el documento se encuentra jerarquizado en una estructura que se llama DOM
+
+    // podemos acceder a un elemento específico por medio del id
+    document.getElementById("parrafito").innerHTML = "<h1> CAMBIE PARA SIEMPRE. <h1/>";
+}
+
+function cambiarDatoDeInput(){
+
+    ejemplito = document.getElementById("textito").value
+    alert("ejemplito")
+    document.getElementById("textito").value = "Hola desde JS"
+}
