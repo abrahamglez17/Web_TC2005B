9d5530a23d0a94f56bc606d117c46684558e3c4c
José Abraham González Navarro <65511679+abrahamglez17@users.noreply.github.com>
2022-03-11 09:01:55 -0600

    uso de endpoints, postman y get y post

--- react-node-app/server/index.js
+++ react-node-app/server/index.js
@@ -1,7 +1,40 @@
 // server/index.js
 const express = require("express");
+const fs = require("fs");
+
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+const newGame = {
+    "Game4" : {
+        "title": "Doom Eternal",
+        "genre": "Shooter",
+        "id": 4
+    }
+}
+
+
+app.get("/api", (req, res) => {
+    res.json({message: "Hello from server side!"});
+});
+
+// callback function
+app.get("/videogames", (req, res) => {
+    fs.readFile(__dirname + "/" + "videogames.json", "utf8", (err, data) => {
+        console.log(data);
+        res.end(data);
+    });
+});
+
+app.post("/addGame", (req, res) => {
+    fs.readFile(__dirname + "/" + "videogames.json", "utf8", (req, res) => {
+        data = JSON.parse(data);
+        data["Game4"] = newGame["Game4"];
+        console.log(data);
+        res.end(JSON.stringify(data));
+    });
+});
+
 app.listen(PORT, () => {
  console.log(`Server listening on ${PORT}`);
 });
